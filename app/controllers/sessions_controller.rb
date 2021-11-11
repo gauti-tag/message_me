31c21f4f38e7f3e2be6787cea1e325c2c7b91c22
Tiehoule Gautier <gautier.tiehoule@gmail.com>
2021-11-11 13:16:24 +0000

    Add authentication system and create, destroy sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,29 @@ class SessionsController < ApplicationController
     end
     
     def create
-       # byebug
+        
+       user = User.find_by(username: params[:session][:username])
+       
+       if user && user.authenticate(params[:session][:password])
+           
+           session[:user_id] = user.id
+           flash[:success] = "You have successfully log in"
+           redirect_to root_path
+           
+       else
+           
+           flash.now[:error] = "there was something wrong with your information"
+           render 'new'
+           
+       end
+    end
+    
+    
+    def destroy
+        
+        session[:user_id] = nil
+        flash[:success] = "you have successfully log out"
+        redirect_to login_path
+        
     end
 end
