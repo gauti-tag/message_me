b72c89b2cea1c258a90b2e6626ea74fa40a8536c
Tiehoule Gautier <gautier.tiehoule@gmail.com>
2021-11-11 09:09:03 +0000

    Build message resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord 
     
-    validates :username, presence: true, length: {minimum: 3, maximum: 15}
-    
+    has_many :messages
+    validates :username, presence: true, length: {minimum: 3, maximum: 15},
+                         uniqueness: { case_sensitive: false }
     has_secure_password
     
 end
